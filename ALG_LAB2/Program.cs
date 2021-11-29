f4f5619d2dc8cc6fbcec18dc42667ee0fb9735ad
PryseOlg <oleg.kuts.02@mail.ru>
2021-11-29 20:24:10 +0500

    Initial commit

---
+++ ALG_LAB2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALG_LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
